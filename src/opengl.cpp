caa4f2799175fcb841f223d7ee3b4650fa94db86
Robert Ortner <robert.ortner.aut@gmail.com>
2020-03-30 20:30:50 +0200

    Mesh loading and rendering

--- src/opengl.cpp
+++ src/opengl.cpp
@@ -22,6 +22,7 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
+#pragma comment(lib, "assimpd.lib")
 #include <assimp/Importer.hpp>
 #include <assimp/scene.h>
 #include <assimp/postprocess.h>
@@ -45,11 +46,14 @@ struct Vertex {
     glm::vec3 position;
     glm::vec3 normal;
     glm::vec2 tex_coords;
+    glm::vec3 tangent;
+    glm::vec3 bitangent;
 };
 
 struct Texture {
     u32 id;
     std::string type; // @todo Maybe a enum could be used?
+    std::string path;
 };
 
 struct Mesh {
@@ -82,6 +86,12 @@ void init_mesh(Mesh *mesh) {
     // Vertex texture coords
     glEnableVertexAttribArray(2);
     glVertexAttribPointer(2, 2, GL_FLOAT, GL_FLOAT, sizeof(Vertex), (void *)offsetof(Vertex, tex_coords));
+    // Vertex tangent
+    glEnableVertexAttribArray(3);
+    glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *)offsetof(Vertex, tangent));
+    // Vertex Bitangent
+    glEnableVertexAttribArray(4);
+    glVertexAttribPointer(4, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *)offsetof(Vertex, bitangent));
     
     glBindVertexArray(0);
 }
@@ -100,6 +110,8 @@ Mesh make_mesh(std::vector<Vertex> vertices, std::vector<u32> indices, std::vect
 void draw_mesh(Mesh *mesh, Shader shader) {
     u32 diffuse_map_count  = 1;
     u32 specular_map_count = 1;
+    u32 normal_map_count   = 1;
+    u32 height_map_count   = 1;
     for (u32 i = 0; i < mesh->textures.size(); ++i) {
         glActiveTexture(GL_TEXTURE0 + i);
         std::string number;
@@ -110,16 +122,182 @@ void draw_mesh(Mesh *mesh, Shader shader) {
         else if (name == "texture_specular") {
             number = std::to_string(specular_map_count++);
         }
+        else if (name == "texture_normal") {
+            number = std::to_string(normal_map_count++);
+        }
+        else if (name == "texture_height") {
+            number = std::to_string(height_map_count++);
+        }
         
-        set_uniform(shader, ("material." + name + number).c_str(), (float)i);
+        glUniform1i(glGetUniformLocation(shader.id, (name + number).c_str()), i);
         glBindTexture(GL_TEXTURE_2D, mesh->textures[i].id);
     }
-    glActiveTexture(GL_TEXTURE0);
     
     // Draw mesh
     glBindVertexArray(mesh->vao);
     glDrawElements(GL_TRIANGLES, mesh->indices.size(), GL_UNSIGNED_INT, 0);
     glBindVertexArray(0);
+    
+    glActiveTexture(GL_TEXTURE0);
+}
+
+struct Model {
+    std::vector<Texture> textures_loaded;
+    std::vector<Mesh> meshes;
+    std::string directory;
+};
+
+void draw_model(Model *model, Shader shader) {
+    for (u32 i = 0; i < model->meshes.size(); ++i) {
+        draw_mesh(&model->meshes[i], shader);
+    }
+}
+
+u32 texture_from_file(const char *path, const std::string &directory, bool gamma = false) {
+    std::string filename = std::string(path);
+    filename = directory + '/' + filename;
+    
+    u32 textureID;
+    glGenTextures(1, &textureID);
+    
+    int width, height, nrComponents;
+    u8 *data = stbi_load(filename.c_str(), &width, &height, &nrComponents, 0);
+    if (data)
+    {
+        GLenum format;
+        if (nrComponents == 1)
+            format = GL_RED;
+        else if (nrComponents == 3)
+            format = GL_RGB;
+        else if (nrComponents == 4)
+            format = GL_RGBA;
+        
+        glBindTexture(GL_TEXTURE_2D, textureID);
+        glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+        
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        
+        stbi_image_free(data);
+    }
+    else
+    {
+        std::cout << "Texture failed to load at path: " << path << std::endl;
+        stbi_image_free(data);
+    }
+    
+    return textureID;
+}
+
+std::vector<Texture> load_material_textures(Model *model, aiMaterial *mat, aiTextureType type, std::string type_name) {
+    std::vector<Texture> textures;
+    for (u32 i = 0; i < mat->GetTextureCount(type); ++i) {
+        aiString str;
+        mat->GetTexture(type, i, &str);
+        bool skip = false;
+        for (u32 j = 0; j < model->textures_loaded.size(); ++j) {
+            if (std::strcmp(model->textures_loaded[j].path.data(), str.C_Str()) == 0) {
+                textures.push_back(model->textures_loaded[j]);
+                skip = true;
+                break;
+            }
+        }
+        if (!skip) {
+            Texture texture;
+            texture.id = texture_from_file(str.C_Str(), model->directory);
+            texture.type = type_name;
+            texture.path = str.C_Str();
+            
+            textures.push_back(texture);
+            model->textures_loaded.push_back(texture);
+        }
+    }
+    return textures;
+}
+
+Mesh process_mesh(Model *model, aiMesh *mesh, const aiScene *scene) {
+    std::vector<Vertex> vertices;
+    std::vector<u32> indices;
+    std::vector<Texture> textures;
+    
+    for (u32 i = 0; i < mesh->mNumVertices; ++i) {
+        Vertex vertex = {};
+        
+        glm::vec3 position;
+        position.x = mesh->mVertices[i].x;
+        position.y = mesh->mVertices[i].y;
+        position.z = mesh->mVertices[i].z;
+        vertex.position = position;
+        
+        glm::vec3 normal;
+        normal.x = mesh->mNormals[i].x;
+        normal.y = mesh->mNormals[i].y;
+        normal.z = mesh->mNormals[i].z;
+        
+        if (mesh->mTextureCoords[0]) {
+            glm::vec2 tex_coords;
+            tex_coords.x = mesh->mTextureCoords[0][i].x;
+            tex_coords.y = mesh->mTextureCoords[0][i].y;
+            vertex.tex_coords = tex_coords;
+        }
+        else {
+            vertex.tex_coords = glm::vec2(0.0f, 0.0f);
+        }
+        
+        vertices.push_back(vertex);
+    }
+    
+    for (u32 i = 0; i < mesh->mNumFaces; ++i) {
+        aiFace face = mesh->mFaces[i];
+        for (u32 j = 0; j < face.mNumIndices; ++j) {
+            indices.push_back(face.mIndices[j]);
+        }
+    }
+    
+    if (mesh->mMaterialIndex >= 0) {
+        aiMaterial *material = scene->mMaterials[mesh->mMaterialIndex];
+        
+        std::vector<Texture> diffuse_maps = load_material_textures(model, material, aiTextureType_DIFFUSE, "texture_diffuse");
+        textures.insert(textures.end(), diffuse_maps.begin(), diffuse_maps.end());
+        
+        std::vector<Texture> specular_maps = load_material_textures(model, material, aiTextureType_SPECULAR, "texture_specular");
+        textures.insert(textures.end(), specular_maps.begin(), specular_maps.end());
+    }
+    
+    Mesh result = make_mesh(vertices, indices, textures);
+    return result;
+}
+
+void load_model_process_node(Model *result, aiNode *node, const aiScene *scene) {
+    // Process all the node's meshes
+    for (u32 i = 0; i < node->mNumMeshes; ++i) {
+        aiMesh *mesh = scene->mMeshes[node->mMeshes[i]];
+        result->meshes.push_back(process_mesh(result, mesh, scene));
+    }
+    // Process child notes
+    for (u32 i = 0; i < node->mNumChildren; ++i) {
+        load_model_process_node(result, node->mChildren[i], scene);
+    }
+}
+
+Model load_model(std::string path) {
+    Model result = {};
+    
+    Assimp::Importer importer;
+    const aiScene *scene = importer.ReadFile(path, aiProcess_Triangulate | aiProcess_FlipUVs);
+    if (!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode) {
+        std::cout << "ERROR::ASSIMP::" << importer.GetErrorString() << std::endl;
+        return {};
+    }
+    result.directory = path.substr(0, path.find_last_of('/'));
+    
+    
+    load_model_process_node(&result, scene->mRootNode, scene);
+    
+    return result;
 }
 
 
@@ -429,15 +607,29 @@ int main() {
 #endif
     
     
+    // @note: Vertex shader
+    const char *model_vs_source =
+#include "model_vs.glsl"
+    ;
+    // @note: Fragment shader
+    const char *model_fs_source =
+#include "model_fs.glsl"
+    ;
+    Shader model_shader = create_shader(model_vs_source, model_fs_source);
+    Model nanosuit_model = load_model("../assets/nanosuit/nanosuit.obj");
+    
+    
     while (!glfwWindowShouldClose(window)) {
         // @note: Handle events / input
         glfwPollEvents();
         process_input(window);
         
         // @note: Update and Render
-        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+        // glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+        glClearColor(0.8f, 0.8f, 0.8f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         
+#if 0
         use_shader(lighting_shader);
 		set_uniform(lighting_shader, "view_pos", camera.position);
 		set_uniform(lighting_shader, "material.shininess", 32.0f);
@@ -528,6 +720,23 @@ int main() {
             set_uniform(lamp_shader, "model_matrix", model_matrix);
             glDrawArrays(GL_TRIANGLES, 0, 36);
         }
+#endif
+        
+        use_shader(model_shader);
+        
+        glm::mat4 view_matrix = get_view_matrix(&camera);
+        set_uniform(model_shader, "view", view_matrix);
+        
+        glm::mat4 projection_matrix = glm::perspective(glm::radians(camera.fov), (f32)SCREEN_WIDTH / (f32)SCREEN_HEIGHT, 0.1f, 100.0f);
+        set_uniform(model_shader, "projection", projection_matrix);
+        
+        glm::mat4 model_matrix = glm::mat4(1.0f);
+        model_matrix = glm::translate(model_matrix, glm::vec3(0.0f, -1.75f, 0.0f));
+        model_matrix = glm::scale(model_matrix, glm::vec3(0.2f, 0.2f, 0.2f));
+        set_uniform(model_shader, "model", model_matrix);
+        
+        draw_model(&nanosuit_model, model_shader);
+        
         
         glfwSwapBuffers(window);
         
